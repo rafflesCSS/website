11d9435d2b97e8df0b97e2c088b231844bef7c9e
Nyx Iskandar <xyntechx@gmail.com>
2021-09-07 11:40:29 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -58,17 +58,17 @@ export default function Home() {
             <div className={utilStyles.slides}>
                 <div name="slide-1" id="slide-1" className={utilStyles.slide}>
                     <blockquote>"CSS is an enriching CCA for all who are passionate about programming! The courses it offers are useful and interesting, and the incubator programme, in particular, promotes creativity and presents the chance for members to experiment and create innovative projects. By and large, anyone who wants to learn computer science should definitely try out for CSS!"</blockquote>
-                    <p>~ Nyx Audrey Angelo Iskandar ~</p>
+                    <p>Nyx Audrey Angelo Iskandar</p>
                 </div>
                 
                 <div name="slide-2" id="slide-2" className={utilStyles.slide}>
                   <blockquote>"CSS is a place where you explore new possibilities and expand your creativity."</blockquote>
-                  <p>~ Lee Baoyu ~</p>
+                  <p>Lee Baoyu</p>
                 </div>
                 
                 <div id="slide-3" className={utilStyles.slide}>
                   <blockquote>"CSS is love. CSS is life."</blockquote>
-                  <p>~ Huang Ziwen ~</p>
+                  <p>Huang Ziwen</p>
                 </div>
             </div>
         </div>
